1f273c5be500b42a9181b51d053001f3ccf8bb3b
Eyal <141010573+eyalm2000@users.noreply.github.com>
2026-05-05 13:12:36 +0300

    add description and custom DNS provider support

--- app/src/main/java/com/eyalm/adns/data/models/DnsProvider.kt
+++ app/src/main/java/com/eyalm/adns/data/models/DnsProvider.kt
@@ -4,11 +4,13 @@ sealed class DnsProvider {
 
     abstract val id: String
     abstract val name: String
+    abstract val description: String
     abstract val isEnhanced: Boolean
 
     data class Standard(
         override val id: String,
         override val name: String,
+        override val description: String,
         val hostname: String      //"dns.adguard-dns.com" etc
     ) : DnsProvider() {
         override val isEnhanced = false
@@ -17,9 +19,20 @@ sealed class DnsProvider {
     data class Enhanced(
         override val id: String,
         override val name: String,
-        val hostname: String      // dynamic from apiService
+        override val description: String,
+        val hostname: String?      // dynamic from apiService
     ) : DnsProvider() {
         override val isEnhanced = true
+    }
+
+
+    data class Custom(
+        val userUrl: String
+    ) : DnsProvider() {
+        override val id = "custom"
+        override val name = "Custom DNS Hostname"
+        override val description = "Use a custom DNS hostname"
+        override val isEnhanced = false
     }
 }
 
